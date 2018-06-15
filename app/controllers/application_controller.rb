d2b08e333144e4a3d227648209eddf4adfedad67
MayaCoro <mayascoro@gmail.com>
2018-06-15 18:27:53 +0000

    we love MEMES

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,10 @@ class ApplicationController < Sinatra::Base
   end
   
   post "/" do 
-    erb :results
     # make an array with each item being a params
-    @my_results_array=[params[question1],params[question2],params[question3],params[question4],params[question5],params[question6]]
+    @my_results_array=[params[:question1],params[:question2],params[:question3],params[:question4],params[:question5],params[:question6]]
     @results = biggest_value(counter(@my_results_array))
+     erb :results
   end
   
 end
